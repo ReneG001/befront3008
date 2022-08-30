721c85d5493f3b7a1d97a800625b39b6ccd6aee4
Rene G <106509901+ReneG001@users.noreply.github.com>
2022-08-30 18:15:06 +0200

    Add files via upload

---
+++ src/pages/Kontakt.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Footer from "../components/Footer.js";
+import Navbar from "../components/Navbar.js";
+import Banner from "../components/Banner.js";
+import heroes from "../pages/images/heroes.jpg";
+
+export default function Kontakt() {
+  return (
+    <div>
+      <Navbar />
+      <Banner title="Kontakt" />
+      <div className="container text-left">
+        <div className="row">
+          <div className="col-md-6">
+            <img src={heroes} alt="Lolli" width="420" height="420" />
+          </div>
+          <div className="col-md-6">
+            <h2>Sugary Heroes - bei Diabetis stets an deiner Seite...</h2>
+            <p>
+              <a href="https://github.com/PhilRichert">Philip</a> | <a href="https://github.com/ReneG001" >René</a> | <a href="https://github.com/romanomantek">Romano</a>
+            </p>
+            <hr />
+            <h2>Legal Informations</h2>
+            <p>
+              Das ist ein Entwickler-Projekt und wir waren zu faul sämtliche
+              Urheberquellen zu notieren. Bei Fragen: nicht fragen.
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
---
+++ src/pages/Lebensmittelhinzu.js
@@ -0,0 +1,63 @@
+import React from "react";
+import Footer from "../components/Footer.js";
+import Navbar from "../components/Navbar.js";
+import Banner from "../components/Banner.js";
+import { Link } from "react-router-dom";
+import icon1 from "../pages/images/icon1.png";
+import icon2 from "../pages/images/icon2.png";
+import icon3 from "../pages/images/icon3.png";
+import "./Main.css";
+
+export default function LebensmittelHinzu() {
+  return (
+    <div>
+      <Navbar />
+      <Banner title="Lebensmittel hinzufügen" />
+      <div className="container text-left">
+        <div className="row">
+          <div className="col-md-5">
+            <h2>Kannste füllen</h2>
+          </div>
+          <div className="col-md-5">
+            <h2>Kannste füllen</h2>
+          </div>
+          <div className="col-md-2 thesidebar">
+            <Link className="nav-link" to="/lebensmittelliste">
+              <img
+                src={icon3}
+                className="icon"
+                alt="Hinzufügen"
+                width="150"
+                height="100"
+              />
+              Lebensmittel auswählen
+            </Link>
+            <hr />
+            <Link className="nav-link" to="/meinemenues">
+              <img
+                src={icon1}
+                className="icon"
+                alt="Hinzufügen"
+                width="150"
+                height="100"
+              />
+              Meine Menüs
+            </Link>
+            <hr />
+            <Link className="nav-link" to="/lebensmittelhinzu">
+              <img
+                src={icon2}
+                className="icon"
+                alt="Hinzufügen"
+                width="150"
+                height="100"
+              />
+              Hinzufügen
+            </Link>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
